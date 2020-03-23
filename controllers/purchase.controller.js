84df64d488ae9b40e4de7c832af78841c76af24a
Vitorhugo17 <martinsvitor11@gmail.com>
2020-03-23 11:54:13 +0000

    fad

--- controllers/purchase.controller.js
+++ controllers/purchase.controller.js
@@ -4,24 +4,14 @@ const connection = require('./../config/connection');
 
 function getPurchase(request, response) {
     let user_id = 151;
-    let contacts = "";
-
+    
     let options = {
-        url: `https://api.hubapi.com/contacts/v1/lists/all/contacts/all?hapikey=${connection.hubspot.key}`
+        url: `https://api.hubapi.com/contacts/v1/contact/vid/${user_id}/profile?hapikey=${connection.hubspot.key}`
     }
     req.get(options, (err, res) => {
         if (!err && res.statusCode == 200) {
-            contacts = JSON.parse(res.body).contacts;
-            let c = [];
-            for (let i = 0; i < contacts.length; i++) {
-                if (contacts[i]["identity-profiles"][0].vid == user_id) {
-                    c.push({
-                        "contact": contacts[i],
-                        "i": i
-                    });
-                }
-            }
-            response.send(JSON.stringify(c));
+            let user = JSON.parse(res.body);
+            response.send(user.properties);
         }
     })
 }
