1fe37861ef0623366fbfb93cce937dfa0d942d46
Vitorhugo17 <martinsvitor11@gmail.com>
2020-03-31 14:05:54 +0100

    remover Exemplo

--- Exemplo/routes/main.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const controllerExample = require('./../controllers/example.controller')
-
-router.get('/', controllerExample.getHome);
-router.get('/pass', controllerExample.comparePass);
-
-module.exports = router;
