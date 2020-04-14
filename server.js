d2dd23fab195d8602bd7c445f23b8a50f49fd90a
Vitorhugo17 <martinsvitor11@gmail.com>
2020-04-14 14:34:11 +0100

    alt

--- server.js
+++ server.js
@@ -68,9 +68,7 @@ app.use(passport.session());
 
 //CORS
 app.use('/', function (request, response, next) {
-    response.header("Access-Control-Allow-Origin", "http://localhost:4242");
     response.header("Access-Control-Allow-Origin", "https://isicampus.herokuapp.com");
-    response.header("Access-Control-Allow-Origin", "https://test-isicampus.herokuapp.com");
     response.header("Access-Control-Allow-Credentials", true);
     response.header("Access-Control-Allow-Methods", "PUT, POST, OPTIONS, GET");
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
